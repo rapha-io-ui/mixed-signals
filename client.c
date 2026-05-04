8d26f9becd6427c90354c38024391e2c3b9b002a
Raphael Andrei Seguenza <aelseguenza20@gmail.com>
2026-05-04 14:34:05 -0400

    Update client.c

--- client.c
+++ client.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -53,37 +54,43 @@ int main(int argc, char *argv[]) {
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons((uint16_t)port_no);
 
-    if (connect(client_sock, (struct sockaddr *)&server_addr,
-                sizeof(server_addr)) < 0) {
+    if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         die_with_error("connect");
     }
 
-    printf("Connected to server.\n");
-    printf("Type your replies when prompted.\n\n");
+    printf("\nConnected to Mixed Signals server.\n");
+    printf("Answer the server prompts, then press Enter. Live updates will appear here.\n\n");
+    printf("Typing before your turn is safe, but the server will ignore early input.\n\n");
+    fflush(stdout);
 
     char buffer[BUFFER_SIZE];
     char input[BUFFER_SIZE];
 
     while (1) {
-        bzero(buffer, sizeof(buffer));
-        ssize_t n = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(client_sock, &readfds);
+        FD_SET(STDIN_FILENO, &readfds);
 
-        if (n < 0) die_with_error("recv");
-        if (n == 0) {
-            printf("\nServer closed the connection.\n");
-            break;
+        int maxfd = (client_sock > STDIN_FILENO) ? client_sock : STDIN_FILENO;
+        int ready = select(maxfd + 1, &readfds, NULL, NULL, NULL);
+        if (ready < 0) die_with_error("select");
+
+        if (FD_ISSET(client_sock, &readfds)) {
+            ssize_t n = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
+            if (n < 0) die_with_error("recv");
+            if (n == 0) {
+                printf("\nServer closed the connection.\n");
+                break;
+            }
+            buffer[n] = '\0';
+            printf("%s", buffer);
+            fflush(stdout);
         }
 
-        buffer[n] = '\0';
-        printf("%s", buffer);
-        fflush(stdout);
-
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == ' ') {
+        if (FD_ISSET(STDIN_FILENO, &readfds)) {
             if (fgets(input, sizeof(input), stdin) == NULL) break;
-
-            if (send_all(client_sock, input) < 0)
-                die_with_error("send");
+            if (send_all(client_sock, input) < 0) die_with_error("send");
         }
     }
 
